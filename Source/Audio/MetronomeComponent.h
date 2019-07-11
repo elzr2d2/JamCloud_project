3a8aded509c929fa04218df4b2b16f98808aa883
elzr2d2 <44233980+elzr2d2@users.noreply.github.com>
2019-07-11 17:14:10 +0300

    added

---
+++ Source/Audio/MetronomeComponent.h
@@ -0,0 +1,41 @@
+/*
+  ==============================================================================
+
+    Metronome.h
+    Created: 13 Apr 2019 3:29:47pm
+    Author:  Joshua Hodge
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "JuceHeader.h"
+
+class MetronomeComponent
+{
+public:
+	MetronomeComponent();
+    
+    // public methods
+    void prepareToPlay (int samplesPerBlock, double sampleRate);
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill);
+    void reset();
+
+	void setBpm(double bpm=132.0);
+	double getBpm();
+    
+private:
+	double bpm = 132.0;
+    int mTotalSamples { 0 };
+    double mSampleRate { 0 };
+    double mBpm { getBpm() };
+    int mUpdateInterval { 0 };
+    int mSamplesRemaining { 0 };
+    
+    AudioFormatManager mFormatManager;
+    std::unique_ptr <AudioFormatReaderSource> pMetronomeSample { nullptr };
+
+
+};
+
