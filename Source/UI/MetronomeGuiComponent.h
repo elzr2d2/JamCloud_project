e742c2f6a137e69e887183b696fcdf84c0587fbb
elzr2d2 <44233980+elzr2d2@users.noreply.github.com>
2019-07-11 17:13:42 +0300

    add metronome manually

---
+++ Source/UI/MetronomeGuiComponent.h
@@ -0,0 +1,47 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "JuceHeader.h"
+#include "Audio/MetronomeComponent.h"
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MetronomeGuiComponent : public AudioAppComponent
+{
+public:
+    //==============================================================================
+	MetronomeGuiComponent();
+    ~MetronomeGuiComponent();
+
+    //==============================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
+    void releaseResources() override;
+
+
+  
+	bool getStatePlay();
+	void setStatePlay(bool statePlay);
+
+	void setBpm(double bpm);
+	double getBpm();
+
+private:
+	double bpm = getBpm();
+	bool statePlay = false;
+
+	MetronomeComponent metronome;
+
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MetronomeGuiComponent)
+};
